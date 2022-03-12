01fb3a444b77566870d745828e9e5c23010629e7
Uekiz <kingghostdragon@gmail.com>
2022-03-12 20:46:11 +0700

    test google login - facebook login

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
+import React from 'react';
+// import FontAwesome from 'font-awesome'
+
+axios.interceptors.request.use(function (config) {
+  const token = sessionStorage.getItem('access_token')
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
+  return config
+}, function (err) {
+  return Promise.reject(err)
+})
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
+
+const responseFacebook = async (response) => {
+  console.log(response);
+  if (response.accessToken) {
+    console.log('log in with accessToken=' + response.accessToken);
+    let result = await axios.post('http://localhost:8080/api/login', {
+      token: response.accessToken
+    })
+    console.log(result.data)
+    sessionStorage.setItem('access_token', result.data.access_token)
+  }
+}
+
+const callInfoAPI = async () => {
+  let result = await axios.get('http://localhost:8080/api/info')
+  console.log(result.data)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div>
+      <GoogleLogin
+        clientId={'967709052865-n4vi83vdl1sdf79gh9coickriebopfmp.apps.googleusercontent.com'}
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      >
+        {/* <FontAwesome
+          name='google'
+        /> */}
+        <span> Login with Google</span>
+      </GoogleLogin>
+    </div><div className="App">
+        <FacebookLogin
+          appId="492042448972691"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={responseFacebook} />,
+        <button onClick={callInfoAPI}>Click me</button>
+      </div></>
   );
 }
 
--- server/main.js
+++ server/main.js
@@ -1,11 +1,58 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+const cors = require('cors')
 const app = express()
-const port = 3000
+const port = 8080
+
+
+app.use(cors())
+
+const TOKEN_SECRET = '870d1fa5011e4d963addea0978c52b321fdc694f9d3632fa9c343f080a5c1d76ee097ce353f9bfe5add221d8285e49a0a1716746f1911c5b3fd1bdf777fde11c'
+var jwt = require('jsonwebtoken');
+
+const authtenticated = (req, res, next) => {
+  const auth_header = req.headers['authorization']
+  const token = auth_header && auth_header.split(' ')[1]
+  if(!token){
+      return res.sendStatus(401)
+  }
+  jwt.verify(token, TOKEN_SECRET, (err, info) => {
+      if(err){
+          return res.sendStatus(403)
+      }
+      req.username = info.username
+      next()
+  })
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/api/login', bodyParser.json(), async (req, res) => {
+  let token = req.body.token
+  let result = await axios.get('https://graph.facebook.com/me', {
+    params: {
+      fields: 'id,name,email',
+      access_token: token
+    }
+  })
+  if (!result.data.id) {
+    res.sendStatus(403)
+    return
+  }
+  let data = {
+    username: result.data.email
+  }
+  let access_token = jwt.sign(data, TOKEN_SECRET, { expiresIn: '1800s' })
+  res.send({ access_token, username: data.username })
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server listening on port ${port}`)
+})
+
+app.get('/api/info', authtenticated, (req, res)  => {
+  res.send({ok: 1, username: req.username})
 })
