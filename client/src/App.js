cdbcf228b2903d20d5f60e42ad11d5d79406376f
Uekiz <kingghostdragon@gmail.com>
2022-03-13 21:41:44 +0700

    try api buy invalid req : redirect uri

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 import FacebookLogin from 'react-facebook-login';
 import GoogleLogin from 'react-google-login';
 import React from 'react';
-// import FontAwesome from 'font-awesome'
 
 axios.interceptors.request.use(function (config) {
   const token = sessionStorage.getItem('access_token')
@@ -43,11 +42,10 @@ function App() {
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
       >
-        {/* <FontAwesome
-          name='google'
-        /> */}
         <span> Login with Google</span>
       </GoogleLogin>
+      {/* ลองใช้ api แต่ invalid_request เกี่ยวกับ redirect_uri
+      https://accounts.google.com/signin/oauth/error?authError=Cg9pbnZhbGlkX3JlcXVlc3QS3gEKWW91IGNhbid0IHNpZ24gaW4gdG8gdGhpcyBhcHAgYmVjYXVzZSBpdCBkb2Vzbid0IGNvbXBseSB3aXRoIEdvb2dsZSdzIE9BdXRoIDIuMCBwb2xpY3kgZm9yIGtlZXBpbmcgYXBwcyBzZWN1cmUuCgpZb3UgY2FuIGxldCB0aGUgYXBwIGRldmVsb3BlciBrbm93IHRoYXQgdGhpcyBhcHAgZG9lc24ndCBjb21wbHkgd2l0aCBvbmUgb3IgbW9yZSBHb29nbGUgdmFsaWRhdGlvbiBydWxlcy4KICAaWWh0dHBzOi8vZGV2ZWxvcGVycy5nb29nbGUuY29tL2lkZW50aXR5L3Byb3RvY29scy9vYXV0aDIvcG9saWNpZXMjc2VjdXJlLXJlc3BvbnNlLWhhbmRsaW5nIJADKh4KDHJlZGlyZWN0X3VyaRIObG9jYWxob3N0OjMwMDA%3D&client_id=967709052865-n4vi83vdl1sdf79gh9coickriebopfmp.apps.googleusercontent.com */}
     </div><div className="App">
         <FacebookLogin
           appId="492042448972691"
