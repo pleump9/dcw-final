087f4ce3b85deba7c6d8ecf2b66dbbe1e026e568
Uekiz <kingghostdragon@gmail.com>
2022-03-26 01:03:02 +0700

    fix bug db

--- server/src/middleware/upload.js
+++ server/src/middleware/upload.js
@@ -1,5 +1,5 @@
 const multer = require("multer");
-const GridFsStorage = require("multer-gridfs-storage");
+const {GridFsStorage} = require('multer-gridfs-storage');
 
 const storage = new GridFsStorage({
     url: process.env.DB,
